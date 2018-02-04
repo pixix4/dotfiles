975f2baa8e052ba777109dcb541c2c3ce6701803
Lars Westermann <lars-westermann@live.de>
2018-02-04 11:23:47 +0100

    Autodetect touchpad

--- scripts/toggletouchpad.sh
+++ scripts/toggletouchpad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device=14
+device=$(xinput --list | grep "Touchpad" | xargs -n 1 | grep "id=" | sed 's/id=//g')
 
 if xinput list-props $device | grep "Device Enabled ([0-9]*):.*1" > /dev/null
 then
@@ -10,4 +10,4 @@ else
   xinput enable $device
   value="On"
 fi
-notify-send "Trackpad" "$value" -t 500 -i "/home/lars/dotfiles/screen/light/ic_touch_app.png" -h string:x-canonical-private-synchronous:volume
+notify-send "Touchpad" "$value" -t 500 -i "/home/lars/dotfiles/screen/light/ic_touch_app.png" -h string:x-canonical-private-synchronous:volume
